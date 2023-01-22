2c3b7f179a30d22c63d7b3b10a9ff44124c14c37
Chionesu <sapienti.Dei@outlook.com>
2023-01-22 00:21:06 -0500

    Additional Visualizations

--- SANDBOX.R
+++ SANDBOX.R
@@ -9,63 +9,86 @@ plan(sequential)
 plan(tweak(multisession, workers = 5))
 test.evs$evt_graphs$`1` |> vertex.attributes()
 
-test.evs$evt_graphs %<>% map(~{
-	g = .x
-	V(g)$length <- map(V(g), ~{
-		.x$trace |>
-			eval(envir = globalenv()) %$% { end_idx - start_idx }
-		})
-	V(g)$src <- V(g)$name %>% stringi::stri_replace_first_regex("[:][0-9]+", "")
-	g
-})
+test.evs$evt_graphs %<>% furrr::future_map(~{
+		g = .x
+		igraph::V(g)$length <- purrr::map(igraph::V(g), ~{
+			.x$trace |> eval(envir = globalenv()) %$% { end_idx - start_idx }
+			})
+		g
+	}, .options = furrr::furrr_options(scheduling = Inf, seed = TRUE, globals = "BLAH"))
 
-trans_matrix <- test.evs$evt_graphs$`1` %>% get.adjacency(attr = "mGap", names = TRUE, sparse = FALSE)
+trans_matrix <- test.evs$evt_graphs$`1` |> igraph::get.adjacency(attr = "mGap", names = TRUE, sparse = FALSE)
 
 # Transition Matrix
 trans_matrix <- {
 		trans_matrix |>
 		rownames() |>
-		map(~{
+		purrr::map(~{
 			c(from = list(stringi::stri_replace_first_regex(.x, "[:][0-9]+", ""))
-				, colnames(inspect)[trans_matrix[.x, ] != 0] %>%
+				, colnames(trans_matrix)[trans_matrix[.x, ] != 0] %>%
 						stringi::stri_replace_first_regex("[:][0-9]+", "") |>
 							table()
 				)
 			}) |>
 		rbindlist(use.names = TRUE, fill = TRUE) %>% {
-		.[, map(.SD, sum, na.rm = TRUE), by = from
-		][, .SD/sum(.SD), by = from] -> .tmp_obj
+			.[, purrr::map(.SD, sum, na.rm = TRUE), by = from
+			][, .SD/sum(.SD), by = from] -> .tmp_obj
 
-		.tmp_obj[, !"from"] %>%
-			as.matrix() %>%
-			setattr("dimnames", list(.tmp_obj$from, colnames(.)))
-	}
-	}
+			.tmp_obj[, !"from"] %>%
+				as.matrix() %>%
+				setattr("dimnames", list(.tmp_obj$from, colnames(.)))
+		}
+}
 
-markov_viz <- split(trans_matrix, f = rownames(trans_matrix)) %>%
-	imap(~{
-		out = t(.x) %>% as.matrix()
+markov_viz_data <- split(trans_matrix, f = rownames(trans_matrix)) |> imap(~{
+		out = t(.x) |> as.matrix()
 
-		walk(purrr::set_names(1:30), ~{
+		purrr::walk(purrr::set_names(1:30), ~{
 				out <<- rbind(out, (out[nrow(out), ] %*% trans_matrix))
 			})
 
-		out %<>% .[, order(colnames(.))]
+		out[, order(colnames(out))]
+	}) |>
+	purrr::imap(~data.table(from_src = .y, .x[c(TRUE, matrixStats::colDiffs(.x) |> apply(1, purrr::as_mapper(~all(round(.x, 3) != 0)))), ] %>% as.data.table())) |>
+	rbindlist() |>
+	melt(id.vars = "from_src", variable.name = "to_src", variable.factor = FALSE)
 
-		plot_ly(
-			x = sprintf("From %s: To %s", .y, colnames(out))
-			, y = ~1:nrow(out)
-			, z = out[-1, ] * (colDiffs(out) %>% round(4) != 0)
-			, type = "contour"
-			, name = .y
-			)
-	})
+markov_viz_data[
+	, map(.SD[, .(source = paste0("F_", from_src), target = paste0("T_", to_src))], ~{
+			outer(.x, unique(sort(c(paste0("F_", from_src), paste0("T_", to_src)))), `==`) |> apply(1, which) - 1
+		}) |> append(list(value = value))
+	][order(source, value)] |>
+modify_at(c("source", "target"), ~factor(.x, levels = sort(unique(.x)), ordered = TRUE)) |>
+plotly::plot_ly(
+	type = "sankey"
+	, orientation = "h"
+	, node = {
+			.colors = replicate(markov_viz_data[, length(unique(sort(c(paste0("F_", from_src), paste0("T_", to_src)))))]
+													, rlang::inject(rgb(!!!runif(3, .1, .8)))
+													) |> sort()
+			list(
+	      label = markov_viz_data[, unique(sort(c(paste0("F_", from_src), paste0("T_", to_src))))]
+	      , color = .colors
+	      , pad = 15
+	      , thickness = 20
+	      , line = list(color = .colors, width = 0.5, stroke = I("#000000"))
+	    	)}
+	, link = { list(
+      source = ~source
+      , target = ~target
+      , value =  ~value
+    	)}
+	) |>
+	plotly::layout(
+    xaxis = list(showgrid = FALSE, zeroline = FALSE)
+    , yaxis = list(showgrid = FALSE, zeroline = FALSE)
+    )
 
-rlang::inject(subplot(!!!markov_viz, nrows = 3)) %>%
-			plotly::layout(
-				title = "Steady-State vs Initial Source"
-				, margin = list(t = -.5)
-				)
-
-
-markov_viz
+markov_viz_data[, c(.SD[, .(from_src)], book.of.features::xform.basis_vector(fvec = to_src, avec = value))][, map(.SD, book.of.utilities::calc.rms), by = from_src] %>%
+plotly::plot_ly(
+	x = ~names(.[, !"from_src"])
+	, y = ~from_src
+	, z = ~as.matrix(.[, !"from_src"])
+	, type = "contour"
+	# , mode = "marker"
+	)
--- validation.R
+++ validation.R
@@ -4,26 +4,35 @@ library(purrr)
 library(tictoc);
 library(future);
 #
-make.test_data <- function(j = 5, n = 5, m = 5, dest = globalenv(), .debug = FALSE){
+make.test_data <- function(j = 5, n = 5, m = 5, o = 1:10, dest = globalenv(), .debug = FALSE){
 #' Make Test Data for Validation
 #'
 #' \code{make.test_data} creates several \code{\link[data.table]{data.table}} objects to be used to validate the package functionality
 #' @param j (integer | 5L) The number of unique values for 'k' to generate
-#' @param n (integer | 5L) The number of sources to create (maximum of 5)
+#' @param n (integer | 5L) The number of sources to create (maximum of 10)
 #' @param m (integer | 5L) The maximum number of columns to generate for each created object (maximum of 15): each column represents an attribute not related to time
+#' @param o (integer[] | 1:10) A vector of integers sampled to create end dates from a randomly-generated start date
 #' @param dest (environment) The destination environment object
 #' @param .debug (logical | \code{FALSE}) When \code{TRUE}, additional debugging items are printed
 #'
 #' @return One to \code{n} \code{\link[data.table]{data.table}} objects prefixed as 'test_data'.
 #'
 	j = max(c(3, abs(as.integer(j))));
-	n = max(c(3, abs(as.integer(n)))); ifelse(n > 5, 5, n);
+	n = max(c(3, abs(as.integer(n)))); ifelse(n > 10, 10, n);
 	m = max(c(3, abs(as.integer(m)))); ifelse(m > 15, 15, m);
+	o = { ifelse(rlang::has_length(o, 1L)
+						 , yes = 1:10
+						 , no = ifelse(any(o <= 0)
+						 							, yes = abs(o)
+						 							, no = ifelse(rlang::has_length(o, 2L)
+						 														, yes = `:`(o[1], o[2]) |> sort()
+						 														, no =  o))
+						 )}
 
 	sequence(n) %>%
-	purrr::set_names(paste0("test_data.", stringi::stri_pad_left(., width = 2, pad = "0"))) %>%
+	purrr::set_names(paste0("test_data.", stringi::stri_pad_left(., width = 2, pad = "0")))|>
 	purrr::map(~{
-		set.seed(sample(1:10000, 1));
+		set.seed(sample(.Random.seed, 1));
 		.src = LETTERS[[.x]];
 
 		.out = purrr::map_dfr(c(1:j), ~list(jk = rep.int(.x, sample(10:100, 1, TRUE)), src = .src)) %>% data.table::as.data.table();
@@ -36,7 +45,7 @@ make.test_data <- function(j = 5, n = 5, m = 5, dest = globalenv(), .debug = FAL
 				)));
 
 		.out[
-			, c("date.start", "date.end") := list(.init_date, .init_date + rpois(n = length(jk), lambda = sample(1:100, length(jk), TRUE)))
+			, c("date.start", "date.end") := list(.init_date, .init_date + rpois(n = length(jk), lambda = sample(o, length(jk), TRUE)))
 			][
 			, paste0("X_", stringi::stri_pad_left(sample(30, m), width = 2, pad = "0")) := purrr::map(1:m, ~sample(runif(1E6), .N, TRUE))
 			][
@@ -49,7 +58,7 @@ make.test_data <- function(j = 5, n = 5, m = 5, dest = globalenv(), .debug = FAL
 }
 
 BLAH <- new.env();
-make.test_data(j = 50, n = 5, m = 5, dest = BLAH, .debug = !TRUE);
+make.test_data(j = 50, n = 5, m = 5, o = c(5, 20), dest = BLAH, .debug = !TRUE);
 set.seed(sample(100000, 1));
 
 # ~ Create EVSpace object from test data
@@ -68,10 +77,10 @@ test.evs$
 		, row.filters	= purrr::map(sequence(length(ls(pattern = "^test_data", envir = BLAH))), ~rlang::expr(1==1))
 		, src.mix 		= "combn"
 		, chatty			= TRUE
-		, exclude.mix = { c(
-				evs_exclude.blender("Data.6", c("Data.4", "Data.7"))
-				, evs_exclude.blender("Data.3", c("Data.1", "Data.5"))
-			)}
+		# , exclude.mix = { c(
+		# 		evs_exclude.blender("Data.6", c("Data.4", "Data.7"))
+		# 		, evs_exclude.blender("Data.3", c("Data.1", "Data.5"))
+		# 	)}
 		)$
 	set.data(chatty = TRUE)$
 	set.q_graphs(chatty = TRUE);
@@ -82,13 +91,13 @@ test.evs$.__enclos_env__$private$q_table;
 toc(log = TRUE);
 #
 # ~ Validation #2 :: make.evs_universe() ====
-plan(tweak(multisession, workers = 5));
+plan(tweak(multisession, workers = 8));
 
 tic("EVSpace Universe Validation");
 make.evs_universe(
 	self = test.evs
 	# , mSt >= quantile(mSt, 0.75)
-	, abs(mGap) >= 10
+	, abs(mGap) >= 5
 	, graph.control = { rlang::exprs(
 				igraph::E(g)$title	<- igraph::ends(g, igraph::E(g)) %>% apply(1, paste, collapse = " -> ")
 				, igraph::V(g)$color <- igraph::V(g)$name %>% stringi::stri_split_fixed(":", simplify = TRUE) %>% .[, 1L] %>% {
@@ -96,7 +105,9 @@ make.evs_universe(
 						y = purrr::set_names(unique(x), purrr::map_chr(unique(x), ~rgb(runif(1), runif(1), runif(1))))
 						purrr::map_chr(x, ~names(y)[which(y == .x)])
 					}
-			)}
+				, igraph::V(g)$src <- igraph::V(g)$name %>% stringi::stri_replace_first_regex("[:][0-9]+", "")
+				)
+		}
 	, omit.na = !TRUE
 	, chatty = TRUE
 	);
@@ -107,24 +118,8 @@ test.evs$space[(jk == 4)] %>% View("Space: jk == 4");
 igraph::vertex.attributes(test.evs$evt_graphs$`1`);
 #
 # ~ Validation #3 :: evs_retrace() ====
-evs_retrace(test.evs)
-
-event_graph <- test.evs$evt_graphs$`4`
-igraph::V(event_graph)$trace[[1]] %>% eval(envir = globalenv())
-test.evs$space[
-	(jk == 4) & (src.pair %ilike% "(->) src3")
-	, as_mapper(~{
-			print(.SD[c(.x, .y)])
-			print(from <- epsilon[.x])
-			print(to <- epsilon[.y])
-			atan2(to - from, to) / (pi/4)
-		})(1, 4)
-	]
-
-test.evs$space[
-	(jk == 4) & (src.pair %ilike% "(->) src3")
-	, from_timeframe[1] %>% lubridate::int_aligns()
-	]
+evs_retrace(test.evs);
+(event_graph <- test.evs$evt_graphs$`4`);
 
 # ~ Validation #4 :: visNetwork::visIgraph() ====
 f2ab <- list(theta = 0.1, gravitationalConstant = -5000, centralGravity = 0.0,  avoidOverlap = 1, damping = 0.7);
